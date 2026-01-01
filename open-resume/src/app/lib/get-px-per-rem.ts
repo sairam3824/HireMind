32cba61b63d06dfa2af1bfeb984d5b7e12d18ab4
MARURI SAI RAMA LINGA REDDY <Msrlreddy@outlook.com>
2026-01-01 06:46:29 +0530

    Remove remaining Open-Resume application framework

--- open-resume/src/app/lib/get-px-per-rem.ts
+++
@@ -1,6 +0,0 @@
-export const getPxPerRem = () => {
-  const bodyComputedStyle = getComputedStyle(
-    document.querySelector("body")!
-  ) as any;
-  return parseFloat(bodyComputedStyle["font-size"]) || 16;
-};
